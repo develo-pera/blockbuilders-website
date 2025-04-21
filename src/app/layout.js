e44bc15b3434c5e4675273b26cc0448fa3ad89d7
Petar Popovic <petarpnp@gmail.com>
2025-04-21 21:36:58 +0200

    Fix og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,8 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://blockbuilders.xyz';
+
 export const metadata = {
   title: "BlockBuilders - The genesis edition",
   description:
@@ -19,14 +21,15 @@ export const metadata = {
     title: "BlockBuilders - The genesis edition",
     description:
       "BlockBuilders - The genesis eidtion is an educational board game teaching players how blockchains and blockchain components works. Learn fundementals of blockchains and web3 in a fun way with your friends while playing the game.",
-    images: "/og-image.png",
+    images: "https://blockbuilde.rs/og-image.png",
+    url: siteUrl,
   },
   twitter: {
     card: "summary_large_image",
     title: "BlockBuilders - The genesis edition",
     description:
       "BlockBuilders - The genesis eidtion is an educational board game teaching players how blockchains and blockchain components works. Learn fundementals of blockchains and web3 in a fun way with your friends while playing the game.",
-    images: "/og-image.png",
+    images: "https://blockbuilde.rs/og-image.png",
   },
 };
 
